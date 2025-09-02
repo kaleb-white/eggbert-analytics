d07c85110d4bd100e563980a9f9a2c67bddb9f47
Kaleb White <kalebw02@ad.unc.edu>
2025-09-01 20:10:49 -0400

    Create python post commit hook

---
+++ git_hooks/post_commit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys, os
+
+EGGBERT_ARCHITECTURE_REL_PATH = "../docs/eggbert_architecture.md"
+
+# def get_curr_file_structure_text():
+#     with os.open(os.getcwd)
+
+# def main(): ...
+
+
+if __name__ == "__main__":
+    os.getcwd()
+    # main()
