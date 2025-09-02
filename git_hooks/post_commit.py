ce7a730328c18a4855d97aaa3f55dfef2426f8cc
Kaleb White <kalebw02@ad.unc.edu>
2025-09-01 20:33:25 -0400

    Test commit

--- git_hooks/post_commit.py
+++ git_hooks/post_commit.py
@@ -2,15 +2,22 @@
 
 import sys, os
 
-EGGBERT_ARCHITECTURE_REL_PATH = "../docs/eggbert_architecture.md"
+EGGBERT_ARCHITECTURE_REL_PATH = "\docs\eggbert_architecture.md"
+EGGBERT_ARCHITECTURE_ABSOLUTE_PATH = os.getcwd() + EGGBERT_ARCHITECTURE_REL_PATH
 
-# def get_curr_file_structure_text():
-#     with os.open(os.getcwd)
 
-# def main(): ...
+def get_curr_file_structure_text():
+    with open(EGGBERT_ARCHITECTURE_ABSOLUTE_PATH, "r") as f:
+        print(f.read())
+
+
+def main():
+    if not os.getcwd().endswith("eggbert-analytics"):
+        print(
+            "Please only commit from the eggbert-analytics directory. The hooks are not configured to work from anywhere else."
+        )
 
 
 if __name__ == "__main__":
-    print(os.getcwd())
 
-    # main()
+    main()
