82d5f4421b6639a1597827f79ce9925b0e910f49
Kaleb White <kalebw02@ad.unc.edu>
2025-09-01 21:32:48 -0400

    Test commit

--- git_hooks/post_commit.py
+++ git_hooks/post_commit.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
-import sys, os
+import sys, os, platform
 
 EGGBERT_ARCHITECTURE_REL_PATH = "\docs\eggbert_architecture.md"
 EGGBERT_ARCHITECTURE_ABSOLUTE_PATH = os.getcwd() + EGGBERT_ARCHITECTURE_REL_PATH
 
 SUBFOLDER_CHAR = chr(26)
+FOLDER_SEPERATOR = "\\" if platform.system().lower().find("windows") != -1 else "/"
+
+OUTPUT_LEFT_PADDING = "-  "
+OUTPUT_FOLDER_SPACING = "\t"
 
 
 def get_curr_file_structure_text():
@@ -22,19 +26,54 @@ def get_curr_file_structure_text():
 
 
 def file_structure_text_to_dict(file_structure_text: str):
-    preprocessed = ""
+    preprocessed: list[str] = []
     for line in (
         file_structure_text.replace("\t", SUBFOLDER_CHAR)
         .replace("    ", SUBFOLDER_CHAR)
         .splitlines()
     ):
-        preprocessed += (
-            line.replace("-   ", "").replace("-  ", "").replace("- ", "") + "\n"
-        )
+        preprocessed += [line.replace("-   ", "").replace("-  ", "").replace("- ", "")]
 
-    return preprocessed
-    for line in preprocessed.splitlines():
-        ...
+    def get_num_subfolders(line: str):
+        count = 0
+        for char in line:
+            if char == SUBFOLDER_CHAR:
+                count += 1
+            else:
+                break
+        return count
+
+    def prepend_parent_folder(stack: list[str], folder_name: str) -> str:
+        res = ""
+        for folder in stack:
+            res += "folder"
+            res += FOLDER_SEPERATOR
+        res += folder_name
+        return res
+
+    stack = []
+    res = {}
+    for line in preprocessed:
+        # Get folder depth and remove folder chars
+        count = get_num_subfolders(line)
+        line = line.lstrip(SUBFOLDER_CHAR)
+
+        # Separate at description
+        [folder, description] = line.split(":")
+
+        # Remove top from stack
+        stack = stack[0:count]
+
+        # Append stack to folder name
+        full_folder_name = prepend_parent_folder(stack, folder)
+
+        # Add to dictionary
+        res[full_folder_name] = description
+
+        # Add folder to stack
+        stack.append(folder)
+
+    return res
 
 
 def main():
@@ -48,5 +87,4 @@ def main():
 
 
 if __name__ == "__main__":
-
     main()
