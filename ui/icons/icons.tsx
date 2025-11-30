92400f4d602154ba594ce78391af338688c2d8bf
Kaleb White <kalebw02@ad.unc.edu>
2025-11-30 17:36:26 -0500

    Add user role icons and update create icons script to update icons.tsx as well as raw_icons.tsx

--- ui/icons/icons.tsx
+++ ui/icons/icons.tsx
@@ -1,7 +1,8 @@
 "use client"
+import { AnonymousRaw, AttachRaw, AuthorRaw, BackRaw, ChartRaw, CheckRaw, DangerRaw, DeleteRaw, DownRaw, EnterRaw, ErrorRaw, ExitRaw, FeedRaw, FileRaw, FilterRaw, ForwardRaw, GlobeRaw, InfoRaw, LeftRaw, LockedRaw, MaximizeRaw, MinimizeRaw, NoUserRaw, PinRaw, QuestionRaw, RedoRaw, RespondentRaw, RightRaw, UndoRaw, UpRaw, UploadRaw, WindowRaw, NewRaw, SwitchRaw } from "./raw_icons.tsx";
+//@@@START@@@
+// Don't remove the above line it's for script
 import { JSX, MouseEventHandler, useRef, useState } from "react";
-import { AttachRaw, BackRaw, ChartRaw, CheckRaw, DangerRaw, DeleteRaw, DownRaw, EnterRaw, ErrorRaw, ExitRaw, FeedRaw, FileRaw, FilterRaw, ForwardRaw, GlobeRaw, InfoRaw, LeftRaw, LockedRaw, MaximizeRaw, MinimizeRaw, NewRaw, PinRaw, QuestionRaw, RedoRaw, RightRaw, SwitchRaw, UndoRaw, UploadRaw, UpRaw, WindowRaw } from "./raw_icons";
-
 export type onClickFuncType = (() => MouseEventHandler<HTMLDivElement> | undefined | void) | undefined
 export type IconType = (edgeLengthPx: number, fillHex?: string) => JSX.Element
 export type FaviconProps = {
@@ -28,122 +29,141 @@ function WrapIcon(props: WrapProps) {
     )
 }
 
+// Don't remove the below line it's used for script
+//@@@END@@@
+export function Anonymous(props: FaviconProps) {
+        return <WrapIcon icon={AnonymousRaw} {...props} />
+    }
+
 export function Attach(props: FaviconProps) {
-	return <WrapIcon icon={AttachRaw} {...props} />
-}
+        return <WrapIcon icon={AttachRaw} {...props} />
+    }
+
+export function Author(props: FaviconProps) {
+        return <WrapIcon icon={AuthorRaw} {...props} />
+    }
 
 export function Back(props: FaviconProps) {
-	return <WrapIcon icon={BackRaw} {...props} />
-}
+        return <WrapIcon icon={BackRaw} {...props} />
+    }
 
 export function Chart(props: FaviconProps) {
-	return <WrapIcon icon={ChartRaw} {...props} />
-}
+        return <WrapIcon icon={ChartRaw} {...props} />
+    }
 
 export function Check(props: FaviconProps) {
-	return <WrapIcon icon={CheckRaw} {...props} />
-}
+        return <WrapIcon icon={CheckRaw} {...props} />
+    }
 
 export function Danger(props: FaviconProps) {
-	return <WrapIcon icon={DangerRaw} {...props} />
-}
+        return <WrapIcon icon={DangerRaw} {...props} />
+    }
 
 export function Delete(props: FaviconProps) {
-	return <WrapIcon icon={DeleteRaw} {...props} />
-}
+        return <WrapIcon icon={DeleteRaw} {...props} />
+    }
 
 export function Down(props: FaviconProps) {
-	return <WrapIcon icon={DownRaw} {...props} />
-}
+        return <WrapIcon icon={DownRaw} {...props} />
+    }
 
 export function Enter(props: FaviconProps) {
-    return <WrapIcon icon={EnterRaw} {...props} />
-}
+        return <WrapIcon icon={EnterRaw} {...props} />
+    }
 
 export function Error(props: FaviconProps) {
-	return <WrapIcon icon={ErrorRaw} {...props} />
-}
+        return <WrapIcon icon={ErrorRaw} {...props} />
+    }
 
 export function Exit(props: FaviconProps) {
-	return <WrapIcon icon={ExitRaw} {...props} />
-}
-
-export function File(props: FaviconProps) {
-	return <WrapIcon icon={FileRaw} {...props} />
-}
-
-export function Filter(props: FaviconProps) {
-	return <WrapIcon icon={FilterRaw} {...props} />
-}
-
-export function Forward(props: FaviconProps) {
-	return <WrapIcon icon={ForwardRaw} {...props} />
-}
-
-export function Globe(props: FaviconProps) {
-	return <WrapIcon icon={GlobeRaw} {...props} />
-}
-
-export function Info(props: FaviconProps) {
-	return <WrapIcon icon={InfoRaw} {...props} />
-}
-
-export function Left(props: FaviconProps) {
-	return <WrapIcon icon={LeftRaw} {...props} />
-}
-
-export function Locked(props: FaviconProps) {
-	return <WrapIcon icon={LockedRaw} {...props} />
-}
-
-export function Maximize(props: FaviconProps) {
-	return <WrapIcon icon={MaximizeRaw} {...props} />
-}
-
-export function Minimize(props: FaviconProps) {
-	return <WrapIcon icon={MinimizeRaw} {...props} />
-}
-
-export function Pin(props: FaviconProps) {
-	return <WrapIcon icon={PinRaw} {...props} />
-}
-
-export function Question(props: FaviconProps) {
-	return <WrapIcon icon={QuestionRaw} {...props} />
-}
-
-export function Redo(props: FaviconProps) {
-	return <WrapIcon icon={RedoRaw} {...props} />
-}
-
-export function Right(props: FaviconProps) {
-	return <WrapIcon icon={RightRaw} {...props} />
-}
-
-export function Undo(props: FaviconProps) {
-	return <WrapIcon icon={UndoRaw} {...props} />
-}
-
-export function Up(props: FaviconProps) {
-	return <WrapIcon icon={UpRaw} {...props} />
-}
-
-export function Upload(props: FaviconProps) {
-	return <WrapIcon icon={UploadRaw} {...props} />
-}
-
-export function Window(props: FaviconProps) {
-	return <WrapIcon icon={WindowRaw} {...props} />
-}
-
-export function New(props: FaviconProps) {
-	return <WrapIcon icon={NewRaw} {...props} />
-}
-
-export function Switch(props: FaviconProps) {
-	return <WrapIcon icon={SwitchRaw} {...props} />
-}
+        return <WrapIcon icon={ExitRaw} {...props} />
+    }
 
 export function Feed(props: FaviconProps) {
-	return <WrapIcon icon={FeedRaw} {...props} />
-}
+        return <WrapIcon icon={FeedRaw} {...props} />
+    }
+
+export function File(props: FaviconProps) {
+        return <WrapIcon icon={FileRaw} {...props} />
+    }
+
+export function Filter(props: FaviconProps) {
+        return <WrapIcon icon={FilterRaw} {...props} />
+    }
+
+export function Forward(props: FaviconProps) {
+        return <WrapIcon icon={ForwardRaw} {...props} />
+    }
+
+export function Globe(props: FaviconProps) {
+        return <WrapIcon icon={GlobeRaw} {...props} />
+    }
+
+export function Info(props: FaviconProps) {
+        return <WrapIcon icon={InfoRaw} {...props} />
+    }
+
+export function Left(props: FaviconProps) {
+        return <WrapIcon icon={LeftRaw} {...props} />
+    }
+
+export function Locked(props: FaviconProps) {
+        return <WrapIcon icon={LockedRaw} {...props} />
+    }
+
+export function Maximize(props: FaviconProps) {
+        return <WrapIcon icon={MaximizeRaw} {...props} />
+    }
+
+export function Minimize(props: FaviconProps) {
+        return <WrapIcon icon={MinimizeRaw} {...props} />
+    }
+
+export function NoUser(props: FaviconProps) {
+        return <WrapIcon icon={NoUserRaw} {...props} />
+    }
+
+export function Pin(props: FaviconProps) {
+        return <WrapIcon icon={PinRaw} {...props} />
+    }
+
+export function Question(props: FaviconProps) {
+        return <WrapIcon icon={QuestionRaw} {...props} />
+    }
+
+export function Redo(props: FaviconProps) {
+        return <WrapIcon icon={RedoRaw} {...props} />
+    }
+
+export function Respondent(props: FaviconProps) {
+        return <WrapIcon icon={RespondentRaw} {...props} />
+    }
+
+export function Right(props: FaviconProps) {
+        return <WrapIcon icon={RightRaw} {...props} />
+    }
+
+export function Undo(props: FaviconProps) {
+        return <WrapIcon icon={UndoRaw} {...props} />
+    }
+
+export function Up(props: FaviconProps) {
+        return <WrapIcon icon={UpRaw} {...props} />
+    }
+
+export function Upload(props: FaviconProps) {
+        return <WrapIcon icon={UploadRaw} {...props} />
+    }
+
+export function Window(props: FaviconProps) {
+        return <WrapIcon icon={WindowRaw} {...props} />
+    }
+
+export function New(props: FaviconProps) {
+        return <WrapIcon icon={NewRaw} {...props} />
+    }
+
+export function Switch(props: FaviconProps) {
+        return <WrapIcon icon={SwitchRaw} {...props} />
+    }
+
--- ui/user_actions/user_actions.tsx
+++ ui/user_actions/user_actions.tsx
@@ -2,6 +2,7 @@ import { Container } from "@/ui/container";
 import { useState } from "react";
 import { SignInForm } from "./sign_in_sign_up/sign_in_form";
 import { SignUpForm } from "./sign_in_sign_up/sign_up_form";
+import { Enter } from "../icons/icons";
 
 type UserActions = "signUp" | "signIn"
 export function UserActions({formOnOpen}: {formOnOpen: UserActions}) {
@@ -10,15 +11,20 @@ export function UserActions({formOnOpen}: {formOnOpen: UserActions}) {
     return (
         <Container>
             <div className="flex flex-col gap-2.5 text-xl">
-                <div className="flex flex-row gap-2.5">
-                {userActions.map((action, i) => {
-                    const styles = `hover:text-tertiary cursor-pointer border-b-2 ${action === selectedForm ? "border-tertiary" : "border-primary"}`
-                    return (
-                        <div key={i} className={styles} onClick={() => {setSelectedForm(action)}}>
-                            {action === "signIn" ? "Sign In" : "Sign Up"}
-                        </div>
-                    )
-                })}
+                <div className="flex flex-row">
+                    <div className="flex items-center mr-1">
+                        <Enter edgeLengthPx={24} />
+                    </div>
+                    <div className="flex flex-row gap-2.5">
+                        {userActions.map((action, i) => {
+                            const styles = `hover:text-tertiary cursor-pointer border-b-2 ${action === selectedForm ? "border-tertiary" : "border-primary"}`
+                            return (
+                                <div key={i} className={styles} onClick={() => {setSelectedForm(action)}}>
+                                    {action === "signIn" ? "Sign In" : "Sign Up"}
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
                 {selectedForm === "signIn"? <SignInForm /> : <SignUpForm />}
 
